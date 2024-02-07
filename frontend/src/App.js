4009f1ca5d0637e2752519f43a8cd76d7035733f
zezo <egypte.2200@gmail.com>
2024-02-07 17:24:51 +0200

    add backend deployed link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import HomePage from './Pages/HomePage';
 import ProjectPage from './Pages/ProjectPage/ProjectPage';
--- frontend/src/GraphQl/client.js
+++ frontend/src/GraphQl/client.js
@@ -2,7 +2,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql',
+  // uri: 'http://localhost:8000/graphql',
+  uri: 'https://graphql-simple-project-management-app.onrender.com/graphql',
   cache: new InMemoryCache(),
 })
 
